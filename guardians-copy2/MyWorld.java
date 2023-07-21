9ef6cdef35cc626436d94041891562abee67b61c
amb231143 <amb231143@uvg.edu.gt>
2023-07-21 08:39:42 -0600

    Agregue los objetos
    Cambie los objetos

--- guardians-copy2/MyWorld.java
+++ guardians-copy2/MyWorld.java
@@ -8,7 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+        private int pausa = 0;
+        private int score;
+        private int vida = 0;
+        private int time;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +20,16 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500, 600, 1); 
+        MosPuntaje();
+        MosScore();
+        ShowTime();
+        time = 1000;
+        addObject (new Nave(), 250, 250);
+    }
+    public void updateVida(int point)
+    {
+        vida = vida + point;
+        MosPuntaje();
+        ShowText("Vida : "+ vida, 400,25)
     }
 }
