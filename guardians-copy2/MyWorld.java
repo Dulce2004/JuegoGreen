a2a48ca6f9bc1117ce24c59553ea30353839c371
amb231143 <amb231143@uvg.edu.gt>
2023-07-21 08:48:40 -0600

    errores
    agregue los comentarios en errores

--- guardians-copy2/MyWorld.java
+++ guardians-copy2/MyWorld.java
@@ -20,16 +20,16 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500, 600, 1); 
-        MosPuntaje();
-        MosScore();
-        ShowTime();
+        //MosPuntaje();
+        //MosScore();
+        //ShowTime();
         time = 1000;
         addObject (new Nave(), 250, 250);
     }
     public void updateVida(int point)
     {
         vida = vida + point;
-        MosPuntaje();
-        ShowText("Vida : "+ vida, 400,25)
+        //MosPuntaje();
+        //ShowText("Vida : "+ vida, 400,25);
     }
 }
